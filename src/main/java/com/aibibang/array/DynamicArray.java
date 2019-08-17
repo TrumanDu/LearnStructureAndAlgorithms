4438c02b015b9cff74778cd0a7b3c21dc34beef1
TrumanDu <truman.p.du@newegg.com>
2019-08-17 17:29:50 +0800

    add array

--- src/main/java/com/aibibang/array/DynamicArray.java
+++ src/main/java/com/aibibang/array/DynamicArray.java
@@ -5,5 +5,76 @@ package com.aibibang.array;
  * @date 2019/08/16
  * @description 实现一个支持动态扩容的数组
  */
-public class DynamicArray {
+public class DynamicArray<E> {
+    private Object[] array;
+    private int cursor = 0;
+
+    public DynamicArray() {
+        array = new Object[10];
+    }
+
+    public DynamicArray(int initialCapacity) {
+        array = new Object[initialCapacity];
+    }
+
+
+    public boolean add(E element) {
+        if (cursor > array.length - 1) {
+            //数组扩容
+            growArray();
+        }
+        array[cursor] = element;
+        cursor++;
+
+        return true;
+    }
+
+    public void set(int index, E element) {
+        if (index > array.length - 1) {
+            //数组扩容
+            growArray();
+        }
+    }
+
+    public E get(int index) {
+        if (index > array.length-1) {
+            throw new IllegalArgumentException("指定index的数据不存在");
+        }
+        return array[index] == null ? null : (E) array[index];
+    }
+
+    public void remove(int index) {
+        if (index < array.length-1) {
+            array[index] = null;
+        }
+    }
+
+
+    public int size() {
+        return cursor;
+    }
+
+    /**
+     * 每次扩容oldLength/2
+     */
+    private void growArray() {
+        int oldLength = array.length;
+        int newCapacity = oldLength + (oldLength >> 1);
+        Object[] tempArray = new Object[newCapacity];
+        System.arraycopy(array, 0, tempArray, 0, array.length);
+        array = tempArray;
+    }
+
+
+    public static void main(String[] args) {
+        DynamicArray<String> array = new DynamicArray<>();
+        for (int i = 0; i < 100; i++) {
+            array.add(i + "");
+        }
+        System.out.println(array.size());
+        for (int i = 0; i < 100; i++) {
+            System.out.println(array.get(i));
+        }
+    }
+
 }
--- src/main/java/com/aibibang/array/SortedArray.java
+++ src/main/java/com/aibibang/array/SortedArray.java
@@ -3,9 +3,65 @@ package com.aibibang.array;
 /**
  * @author Truman.P.Du
  * @date 2019/08/16
- * @description
- * 实现一个大小固定的有序数组，支持动态增删改操作
- *
+ * @description 实现一个大小固定的有序数组，支持动态增删改操作
  */
-public class SortedArray {
+public class SortedArray<E> {
+    Object[] array;
+    private int size = 0;
+
+    public SortedArray(int initialCapacity) {
+        array = new Object[initialCapacity];
+    }
+
+    public boolean add(E element) {
+        if (size > array.length - 1) {
+            remove(0);
+        }
+        array[size] = element;
+        size++;
+        return true;
+    }
+
+    public boolean set(int index, E element) {
+        if (index > array.length - 1) {
+            return false;
+        }
+        array[index] = element;
+
+        return true;
+    }
+
+    public E get(int index) {
+        if (index > array.length - 1) {
+            throw new IllegalArgumentException("数组越界！");
+        }
+        return array[index] == null ? null : (E) array[index];
+    }
+
+    public boolean remove(int index) {
+        if (index > array.length - 1) {
+            return false;
+        }
+        //移动数据
+        for (int i = index; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        array[array.length - 1] = null;
+        size--;
+        return true;
+    }
+
+
+
+    public static void main(String[] args) {
+        SortedArray<Integer> array = new SortedArray<>(10);
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 1000_000; i++) {
+            array.add(i);
+        }
+        System.out.println("costs :" + (System.currentTimeMillis() - startTime));
+        for (int i = 0; i < 10; i++) {
+            System.out.println(array.get(i));
+        }
+    }
 }
